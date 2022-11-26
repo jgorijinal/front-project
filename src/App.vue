b4fc20ffe36374c7db5bd5aece5a5a6b698f67b1
jgoriginal <932274204@qq.com>
2022-11-26 15:15:21 +0800

    安装tailwindcss和sass

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,8 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://vuejs.org/" target="_blank">
+    <h1 class="bg-red-900">123456</h1>
+    <a href="https://vuejs.org/" target="_blank" >
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
