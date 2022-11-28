ffe91702e0ca32e03755d0ea16befb15671dfecd
jgoriginal <932274204@qq.com>
2022-11-28 16:39:06 +0800

    创建header, main, floating

---
+++ src/views/layout/components/floating/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    floating
+  </div>
+</template>
+<script setup>
+
+</script>
+<style lang="scss" scoped>
+  
+</style>
---
+++ src/views/layout/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header-vue></header-vue>
+    <main-vue></main-vue>
+    <floating-vue></floating-vue>
+  </div>
+</template>
+<script setup>
+import headerVue from './components/header/index.vue'
+import mainVue from './components/main.vue'
+import floatingVue from './components/floating/index.vue'
+</script>
+<style lang="scss" scoped>
+
+</style>
