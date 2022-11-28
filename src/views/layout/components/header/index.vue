b4429a07a335761e899e1ac0dfd2cb054990453a
jgoriginal <932274204@qq.com>
2022-11-28 17:12:07 +0800

    初始化 header/index.vue

--- src/views/layout/components/header/index.vue
+++ src/views/layout/components/header/index.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>
-    header
+<div class="w-full bg-white border-b border-b-zinc-200 px-2 py-1">
+  <div class="flex items-center">
+    <!--logo-->
+    <img src="@/assets/vue.svg" class="h-4 cursor-pointer mr-2" alt="" @click="onClickLogo">
+    <!--search-->
+    <header-search-vue class="mr-1"></header-search-vue>
+    <!--theme-->
+    <header-theme-vue class="mr-1"></header-theme-vue>
+    <!--my-->
+    <header-my-vue></header-my-vue>
   </div>
+</div>
 </template>
 <script setup>
-
+import headerSearchVue from './header-search/index.vue'
+import headerThemeVue from './header-theme.vue'
+import headerMyVue from './header-my.vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const onClickLogo = () => {
+  router.push('/')
+}
 </script>
 <style lang="scss" scoped>
   
