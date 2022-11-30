c77afe69a6ffae0eeb62d1259684f4bcfb96e367
jgoriginal <932274204@qq.com>
2022-11-30 15:30:01 +0800

    完成pc端 navigationBar的视图处理

--- src/views/layout/components/main.vue
+++ src/views/layout/components/main.vue
@@ -1,11 +1,5 @@
 <template>
-  <div>
-    main
-    <m-button size="small" type="main">main</m-button>
-    <m-button size="small" type="primary">primary</m-button>
-    <m-button size="small" type="info">info</m-button>
-    <m-button size="small" type="main" icon="search"></m-button>
-  </div>
+  <router-view />
 </template>
 <script setup>
 
--- src/views/main/components/navigation/index.vue
+++ src/views/main/components/navigation/index.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <mobile-navigation-vue v-if="isMobileTerminal"/>
+    <pc-navigation-vue v-else/>
   </div>
 </template>
 <script setup>
 import { isMobileTerminal } from '@/utils/flexible'
 import { useStore } from 'vuex';
 import mobileNavigationVue from './mobile/index.vue'
-
+import pcNavigationVue from './pc/index.vue'
 // const categorys = ref([]) // 分类
 // const getCategoryData = async () => {
 //   const res = await getCategory()
