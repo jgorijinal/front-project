d323ed4a137a777666e7864c17a44faebc5a116d
jgoriginal <932274204@qq.com>
2022-12-01 15:31:42 +0800

    创建 list 和 item组件

---
+++ src/views/main/components/list/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    list
+  </div>
+</template>
+<script setup>
+
+</script>
+<style lang="scss" scoped>
+  
+</style>
--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
+    <!--导航栏 pc/移动端-->
     <navigation-vue></navigation-vue>
+    <!--图片列表-->
+    <list-vue></list-vue>
   </div>
 </template>
 <script setup>
 import navigationVue from './components/navigation/index.vue'
+import listVue from './components/list/index.vue'
 </script>
 <style lang="scss" scoped>
   
