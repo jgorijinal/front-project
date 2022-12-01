d898e9bf396328ecbf99e6fbcd19e2673ef081ae
jgoriginal <932274204@qq.com>
2022-12-01 13:09:32 +0800

    跟随系统主题的变化

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -4,18 +4,35 @@ import { watch } from 'vue'
 import store from '@/store'
 import { THEME_LIGHT, THEME_DARK, THEME_SYSTEM } from '@/constants'
 
-export default () => {
-  watch(()=> store.getters.themeType, (newThemeType) => {
-    let themeClassName = ''
+// 监听系统主题变化
+let matchMedia
+const watchSystemThemeChange = () => {
+  // 仅需初始化一次
+  if (matchMedia) return 
+  matchMedia = window.matchMedia('(prefers-color-schema:dark)')
+  // 监听主题变更
+  matchMedia.onchange = () => {
+    changeTheme(THEME_SYSTEM)
+  }
+}
 
-    if (newThemeType === THEME_LIGHT) {
-      themeClassName = 'light'
-    } else if (newThemeType === THEME_DARK) {
-      themeClassName = 'dark'
-    }
-    // 修改 html 元素的 class
-    document.querySelector('html').className = themeClassName
-  }, {
+const changeTheme = (newThemeType) => {
+  let themeClassName = ''
+
+  if (newThemeType === THEME_LIGHT) {
+    themeClassName = 'light'
+  } else if (newThemeType === THEME_DARK) {
+    themeClassName = 'dark'
+  } else if (newThemeType === THEME_SYSTEM) {
+    watchSystemThemeChange()
+    themeClassName = matchMedia.matches ? 'dark' : 'light'
+  }
+  // 修改 html 元素的 class
+  document.querySelector('html').className = themeClassName
+}
+
+export default () => {
+  watch(()=> store.getters.themeType, changeTheme , {
     immediate: true
   }) 
 }
