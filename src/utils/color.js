f78f99e9029739bf41eb6d5a898474e32eecde9c
jgoriginal <932274204@qq.com>
2022-12-03 16:36:57 +0800

    封装生成随机rgb的工具函数

---
+++ src/utils/color.js
@@ -0,0 +1,7 @@
+// 生成随机颜色
+export function randomRGB() {
+  const r = Math.floor(Math.random * 256)
+  const g = Math.floor(Math.random * 256)
+  const b = Math.floor(Math.random * 256)
+  return `rgb(${r},${g},${b})`
+}
