56ce8cb006830aea148b4a3a3401c0fe90faa62e
jgoriginal <932274204@qq.com>
2022-12-03 21:28:35 +0800

    创建 currentCategoryIndex的getters快捷访问

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,4 +1,14 @@
 export default {
+  // navigation 的全部的分类
   categorys: state => state.category.categorys,
-  themeType: state => state.theme.themeType
+  // 主题
+  themeType: state => state.theme.themeType,
+  // navigation 的当前选中项
+  currentCategory: state => state.app.currentCategory,
+  // navigation 的当前选中项的索引 index
+  currentCategory: (state, getters) => {
+    return getters.categorys.findIndex(category => {
+      return category.id === getters.currentCategory.id
+    })
+  }
 }
--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,6 +1,7 @@
 // 这个模块是不被缓存
 import { ALL_CATEGORY_ITEM } from '@/constants'
 export default {
+  namespaced: true,
   state() {
     return {
       // navigation 的当前选中项
