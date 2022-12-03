923cc677222eb13dd96c8c229a2c34dfce820cd6
jgoriginal <932274204@qq.com>
2022-12-03 13:07:17 +0800

    infinite-list 组件 : props 定义

--- src/libs/infinite-list/index.vue
+++ src/libs/infinite-list/index.vue
@@ -4,7 +4,17 @@
   </div>
 </template>
 <script setup>
-
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    required: true
+  },
+  isFinished: {
+    type: Boolean,
+    default:false
+  }
+})
+const emits = defineEmits(['update:modelValue', 'onLoad'])
 </script>
 <style lang="scss" scoped>  
 </style>
