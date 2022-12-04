0ceb3086d2959b64c32e74bed732a431608b4f32
jgoriginal <932274204@qq.com>
2022-12-04 13:04:54 +0800

    定义获取搜素提示的接口

--- src/api/pexels.js
+++ src/api/pexels.js
@@ -1,8 +1,19 @@
 import request from '@/utils/request'
 
+// 获取图片列表
 export function getPexelsList(data) {
   return request({
     url: '/pexels/list',
     params:data
   })
 } 
+
+// 获取搜索提示
+export function getHint(q) {
+  return request({
+    url: '/pexels/hint',
+    params: {
+      q
+    }
+  })
+}
