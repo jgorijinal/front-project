3b6a351b6504ea6c2358b68f3b6530dff9b2fe5b
jgoriginal <932274204@qq.com>
2022-12-04 17:10:23 +0800

    searchBar : 触发搜索时, 调用addHistory的action添加历史记录

--- src/views/layout/components/header/header-search/index.vue
+++ src/views/layout/components/header/header-search/index.vue
@@ -14,10 +14,12 @@
 <script setup>
 import { ref } from 'vue'
 import hintVue from './hint.vue'
+import { useStore } from 'vuex'
 
 // 输入框的内容
 const inputValue = ref('123')
 
+const store = useStore()
 // 点击了搜索按钮
 // TODO : 点击了搜索历史中的某一项  
 const onSearchHandler = (item) => {
@@ -26,11 +28,13 @@ const onSearchHandler = (item) => {
   }
   inputValue.value = item
   console.log('开始搜索', inputValue.value)
+  store.commit('search/addHistory', item )
 }
 // 监听点击了某一个搜索提示
 const hintClickHandler = (item) => {
-  inputValue.value = item
   console.log('开始搜索', inputValue.value)
+  inputValue.value = item
+  store.commit('search/addHistory', item )
 }
 </script>
 <style lang="scss" scoped>
