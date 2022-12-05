2f3d3dc835377277cdab0ba8bac23c332847bf73
jgoriginal <932274204@qq.com>
2022-12-05 22:06:43 +0800

    安装 file-server

--- src/views/main/components/list/item.vue
+++ src/views/main/components/list/item.vue
@@ -15,6 +15,7 @@
       <div class="hidden xl:block opacity-0 hover:opacity-90 duration-300 hover:bg-zinc-800/70
         absolute z-10 left-0 top-0 w-full h-full rounded hover:cursor-zoom-in">
         <!--一些按钮-->
+        <!--分享-->
         <m-button class="absolute top-1.5 left-1.5" type="main">分享</m-button>
         <m-button
           class="absolute top-1.5 right-1.5"
@@ -22,13 +23,16 @@
           icon="heart"
           iconClass="fill-zinc-900 dark:fill-zinc-200"
         />
+        <!--下载-->
         <m-button
           class="absolute bottom-1.5 left-1.5 bg-zinc-100/70"
           type="info"
           size="small"
           icon="download"
           iconClass="fill-zinc-900 dark:fill-zinc-200"
+          @click="onDownload"
         />
+        <!--全屏-->
         <m-button
           class="absolute bottom-1.5 right-1.5 bg-zinc-100/70"
           type="info"
@@ -56,6 +60,10 @@ const props = defineProps({
     type: Number,
   }
 })
+
+const onDownload = () =>{
+  
+}
 </script>
 <style lang="scss" scoped>
 </style>
