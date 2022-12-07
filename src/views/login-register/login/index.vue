27aae9a4ea27e0cd900c756758ee2cbe4f0aa62b
jgoriginal <932274204@qq.com>
2022-12-07 22:04:44 +0800

    使用 vee-validate 实现基本的表单校验需求

--- src/views/login-register/login/index.vue
+++ src/views/login-register/login/index.vue
@@ -32,22 +32,25 @@
         账号登录
       </h3>
       <!-- 表单 -->
-      <form>
-        <input
+      <vee-form @submit="onLoginHandler">
+        <vee-field
           class="dark:bg-zinc-800 dark:text-zinc-400 border-b-zinc-400 border-b-[1px] w-full outline-0 pb-1 px-1 text-base focus:border-b-main dark:focus:border-b-zinc-200 xl:dark:bg-zinc-900"
           name="username"
           type="text"
           placeholder="用户名"
           autocomplete="on"
+          :rules="validateUsername"
         />
-        <input
+        <vee-error-message name="username"  class="text-sm text-red-600 block mt-0.5 text-left"></vee-error-message>
+        <vee-field
           class="dark:bg-zinc-800 dark:text-zinc-400 border-b-zinc-400 border-b-[1px] w-full outline-0 pb-1 px-1 text-base focus:border-b-main dark:focus:border-b-zinc-200 xl:dark:bg-zinc-900"
           name="password"
           type="password"
           placeholder="密码"
           autocomplete="on"
+          :rules="validatePassword"
         />
-
+        <vee-error-message name="password"  class="text-sm text-red-600 block mt-0.5 text-left"></vee-error-message>
         <div class="pt-1 pb-3 leading-[0px] text-right">
           <a
             class="inline-block p-1 text-zinc-400 text-right dark:text-zinc-600 hover:text-zinc-600 dark:hover:text-zinc-400 text-sm duration-400 cursor-pointer"
@@ -59,7 +62,7 @@
         <m-button class="w-full dark:bg-zinc-900 xl:dark:bg-zinc-800">
           登录
         </m-button>
-      </form>
+      </vee-form>
 
       <div class="flex justify-around mt-4">
         <!-- QQ -->
@@ -71,7 +74,13 @@
   </div>
 </template>
 <script setup>
+import { Form as VeeForm, Field as VeeField, ErrorMessage as VeeErrorMessage } from 'vee-validate'
+import { validateUsername, validatePassword } from '../validate'
 
+// 点击登录按钮
+const onLoginHandler = (val) => {
+  console.log(val)
+}
 </script>
 <style lang="scss" scoped>
   
---
+++ src/views/login-register/validate.js
@@ -0,0 +1,26 @@
+/**
+ * 用户明德表单校验
+ * 返回 true 表示表单校验通过 , 返回 字符串 表示表单校验不通过
+ */
+export const validateUsername = (value) => {
+  if (!value) {
+    return '用户名必须要填写!'
+  }
+  if (value.length < 3 || value.length > 12) {
+    return '用户名的长度应该在3 ~ 12位之间'
+  }
+  return true
+}
+/**
+ * 
+ * 密码的比导弹校验
+ */
+export const validatePassword = (value) => {
+  if (!value) {
+    return '密码必须要填写!'
+  }
+  if (value.length < 6 || value.length > 12) {
+    return '密码的长度应该在6 ~ 12位之间'
+  }
+  return true
+}
